38f013f30f3278a6f725240636236e5e8a21ca73
Shiny Alex <carvantes.alex@gmail.com>
2018-08-11 00:58:27 -0500

    one more

---
+++ templates/hollywood.php
@@ -0,0 +1,231 @@
+<?php
+    /* Template Name:  Hollywood*/ 
+    get_header();
+?>
+
+
+
+
+<section class="template-section" id="peel">
+    
+    <div class="container">
+        <h2 class="saqua uptown">Hollywood Peel
+            </h2>
+            
+        <div class="">
+            <div class="w-box">
+            <p>Consiste en la aplicación de una crema de carbón que penetra la capa superficial de  la piel y absorbe un poco de calor. Posteriormente se vaporiza y elimina la capa de células muertas de la piel. 
+            
+            <br><br>Este procedimiento ayuda al rejuvenecimiento del rostro,  es seguro y aplicable a la mayoría de los tipos de piel. 
+            
+            <br><br><b>No necesita tiempo de recuperación ya que no es invasivo, ni doloroso. </b></p>
+            
+            </div>
+        </div>
+        
+    </div>
+     
+</section>
+
+<section id="beneficios">
+    <div class="container">
+        <div class="flex ">
+            <div class="col col-txt" id="faces">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/hollywood/antesdespues.jpg" alt="Antes y después del tratamiento">
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Beneficios
+                </h2>
+                <img class="line" src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul>
+                    <li>
+                        <p>Piel más brillante. </p>
+                    </li>
+                    <li>
+                        <p>Mejora el tono general de la piel.  </p>
+                    </li>
+                    <li>
+                        <p>Reduce o elimina manchas, por sol o 
+                           edad. </p>
+                    </li>
+                    <li>
+                        <p>Sensación de suavidad en zona tratada.</p>
+                    </li>
+                    <li>
+                        <p>Atenúa cicatrices finas (líneas de 
+                           expresión y  acné).</p>
+                    </li>
+                     
+                    <li>
+                            <p>Reducción del tamaño de los poros con
+                           resultados a corto y largo plazo. </p>
+                    </li>
+                    
+                    <li>
+                        <p>Procedimiento cómodo y rápido.</p>
+                    </li>
+                    
+                    <li>
+                            <p>Puede utilizarse antes de un evento
+                       importante (boda, graduación, entre
+                       otros.).</p>
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+
+
+<section id="resultado">
+  <div class="container">
+       <div class="relative-top">
+           <h2 class="uptown center">
+              El resultado final es un aspecto 
+                <br>
+               <span class="big">
+                   fresco y rejuvenecido de la piel.
+               </span>
+           </h2>
+            <div class="flex">
+                <div class="col">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/hollywood/despegar.jpg" alt="Mujer de piel perfecta">
+                   </div>
+                   
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>¿Cómo puede hacerse?</h2>
+                    <p>Para mejores resultados es conveniente realizar varias sesiones consecutivas (mínimo 3).</p>
+                    <p>Las sesiones pueden ser cada 15 días o cada mes, dependiendo de las necesidades de tu piel.</p>
+                    
+                </div>
+                
+                
+            </div>
+       </div>
+   
+  </div>
+</section>
+
+
+
+
+
+<section id="no-en">
+  <div class="container">
+      
+       <div class="relative-top">
+            
+            <div class="flex">
+           
+                <div class="col col-txt">
+                   <div class="">
+                        <h2>No puede utilizarse en: </h2>
+                       <ul>
+                            <li>
+                                <p>Pieles bronceadas o recientemente
+                                   expuestas al sol. </p>
+                            </li>
+                            <li>
+                                <p>En tonos de piel muy oscuros.</p>
+                            </li>
+                             <li>
+                                <p>En pacientes con tratamientos 
+                                   foto-estimulantes <span class="ejemplo">(Ejemplo:  
+                                    Roaccutane).</span> </p>
+                            </li>
+                        </ul>
+                    
+                   </div>
+                    
+                </div>
+                
+                <div class="col">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/hollywood/maskpro.jpg" alt="Aplicación de crema de carbón">
+                   </div>
+                   
+                </div>
+                
+            </div>
+       </div>
+   
+  </div>
+</section>
+
+<section class="pad-top-section" id="howtopay">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="pills">
+                <div class="fcentered"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/creditcard.png" alt="Tarjeta de crédito"></div>
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="">¿Cómo Pagar?
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <p>
+                    Efectivo, debito, crédito. 3 meses sin intereses todas las tarjetas de crédito y todos los bancos.
+                </p>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+<section id="money-brown">
+  <div class="container">
+      
+      
+            
+            <div class="flex">
+           
+                <div class="col col-txt">
+                    <h2>¿Cuánto cuesta?</h2>
+                       
+                    <ul>
+                        <li>
+                            <p><b>1 Sesión: </b><br>2,000 MXN</p>
+                        </li>
+                        <li>
+                            <p><b>3 Sesiones:</b><br>5,100 MXN<br><span class="ejemplo">(Una sesión cada 15 días)</span></p>
+                        </li>
+                    </ul>
+                    
+                    <p><b>Tratamiento mínimo recomendo: 3 sesiones </b></p>
+
+                    
+                </div>
+                
+                <div class="col">
+                   <div class="relative-top">
+                       <div class="fcentered">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/money.png" alt="Dinero en efectivo">
+                       </div>
+                    </div>
+                </div>
+                
+            </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+<?php 
+    get_footer();
+?>
+
