083721c8e3a9ba262418dd03a0698dbc38c0597a
Lyngall <soporte@esbrillante.com>
2018-11-08 10:22:53 -0600

    New section style masajes

---
+++ templates/masaje.php
@@ -0,0 +1,416 @@
+<?php
+    /* Template Name:  masaje*/ 
+echo "<section id ='shiny-template'>";
+    get_header();
+?>
+
+
+
+
+<section class="template-section masaje cream" id="mas1">
+  <div class="container">
+      
+            <div class="flex">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+           <div class="flex">
+               <div class="col col-img"></div>
+               <div class="col col-txt">
+                   
+                    <div class="relative-top">
+                         <a href="http://bodybalance.mx/tratamientos/" class="btn-brown">¡Reservar!</a>
+                    </div>
+                   
+               </div>
+           </div>
+   
+  </div>
+</section>
+
+<section class="template-section masaje aqua-back" id="mas2">
+  <div class="container">
+      
+            <div class="flex reverse">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+           <div class="flex reverse">
+               <div class="col col-img"></div>
+               <div class="col col-txt">
+                   
+                    <div class="relative-top">
+                         <a href="http://bodybalance.mx/tratamientos/" class="btn-brown">¡Reservar!</a>
+                    </div>
+                   
+               </div>
+           </div>
+   
+  </div>
+</section>
+
+<section class="template-section masaje saqua-back" id="mas3">
+  <div class="container">
+      
+            <div class="flex">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+           <div class="flex">
+               <div class="col col-img"></div>
+               <div class="col col-txt">
+                   
+                    <div class="relative-top">
+                         <a href="http://bodybalance.mx/tratamientos/" class="btn-brown">¡Reservar!</a>
+                    </div>
+                   
+               </div>
+           </div>
+   
+  </div>
+</section>
+
+
+<section class="template-section masaje cream" id="mas4">
+  <div class="container">
+      
+            <div class="flex">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+           <div class="flex">
+               <div class="col col-img fcentered">
+                   
+                      
+                    <div class="relative-top">
+                         <a href="http://bodybalance.mx/tratamientos/" class="btn-brown">¡Reservar!</a>
+                    </div>
+                   
+               </div>
+               <div class="col col-txt ">
+                
+                   
+               </div>
+           </div>
+   
+  </div>
+</section>
+
+
+<section class="template-section masaje aqua-back" id="mas5">
+  <div class="container">
+      
+            <div class="flex reverse">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+           <div class="flex reverse">
+               <div class="col col-img"></div>
+               <div class="col col-txt">
+                   
+                    <div class="relative-top">
+                         <a href="http://bodybalance.mx/tratamientos/" class="btn-brown">¡Reservar!</a>
+                    </div>
+                   
+               </div>
+           </div>
+   
+  </div>
+</section>
+
+
+<section class="template-section masaje saqua-back" id="mas6">
+  <div class="container">
+      
+            <div class="flex">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+           <div class="flex">
+               <div class="col col-img"></div>
+               <div class="col col-txt">
+                   
+                    <div class="relative-top">
+                         <a href="http://bodybalance.mx/tratamientos/" class="btn-brown">¡Reservar!</a>
+                    </div>
+                   
+               </div>
+           </div>
+   
+  </div>
+</section>
+
+<section class="template-section masaje cream" id="mas7">
+  <div class="container">
+      
+            <div class="flex">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+
+   
+  </div>
+</section>
+
+<section class="template-section masaje aqua-back" id="mas8">
+  <div class="container">
+      
+            <div class="flex">
+                <div class="col-img">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/masaje-espalda-dosmanos.jpg" alt="Masaje de espalda a dos manos">
+                   </div>
+                </div>
+                
+                <div class="col col-txt">
+                    <h2>
+                        Es un centro integral de nutrición y metabolismo.
+                    </h2>
+                    <p>
+                        Ofrecemos consulta  médico-nutricional, venta de suplementos  y  tratamientos complementarios cómo modelado corporal con ondas de choque tecnología suiza Cellactor SC1®, faciales y depilación laser diodo. 
+                    </p>
+                    
+                    <div class="flex vcentered">
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/clock.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+                            <p>1 hora</p>
+                        </div>
+                        <div class="col icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masaje/money.png" alt="Masaje en abdomen">
+                        </div>
+                        <div class="col txt">
+         
+                                <p>$750.00</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+           </div>
+  
+   
+  </div>
+</section>
+
+
+
+
+
+
+<?php 
+    include("shiny-footer.php");
+    get_footer();
+?>
+
