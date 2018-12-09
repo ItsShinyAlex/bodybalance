3824cdb2c8ab52a2bbd67384a09779c71a73d63b
Lyngall <soporte@esbrillante.com>
2018-12-09 17:39:37 -0600

    radio advance

---
+++ templates/radio.php
@@ -0,0 +1,489 @@
+<?php
+    /* Template Name: radio */ 
+echo "<section id ='shiny-template'>";
+    get_header();
+?>
+
+
+
+
+<section class="template-section" id="office">
+    
+    <div class="container center">
+        <h2 class="saqua uptown">Radiofrecuencia</h2>
+            
+        <div class="fcentered">
+            <div class="w-box">
+            <p>Es un tratamiento estético seguro y eficaz que genera ondas electromagnéticas y con ello, un rápido movimiento de las moléculas de agua del tejido graso.
+            <br><br>
+            Esto eleva la temperatura debido a la fricción y permite la eliminación de la grasa para moldear el cuerpo, mejorar la apariencia de la celulitis y la flacidez de piel. 
+            </p>
+            </div>
+        </div>
+        
+        <div class="fcentered">
+             <div class="desktop-hide relative-top">
+                <a href="#contacto" class="btn-brown">¡ME INTERESA!</a>
+            </div>
+        </div>
+        
+    </div>
+     
+</section>
+
+<section class="template-section" id="obtienes">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="pills">
+                <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/faceARROWS.jpg" alt="Cutis limpio">
+                </div>
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">¿Qué obtienes?
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul>
+                    <li>
+                        <p>Estimula la circulación sanguínea. </p>
+                    </li>
+                    <li>
+                        <p>Mayor eliminación de desechos y mayor
+                           aporte de nutrientes. </p>
+                    </li>
+                    <li>
+                        <p>Favorece el drenaje linfático natural del
+                         cuerpo, eliminando líquidos y toxinas.  </p>
+                    </li>
+                    <li>
+                        <p>Promueve formación de nuevo 
+                           colágeno y elastina en capas profundas
+                           dando mayor tensión en piel. </p>
+                    </li>
+                    <li>
+                        <p>Reduce el volumen en la zona tratada. </p>
+                    </li>
+                    <li>
+                        <p>Reafirma y da frescura al rostro.</p>
+                    </li>
+                    <li>
+                        <p>Reduce la circunferencia de los brazos
+                          y piernas. </p>
+                    </li>
+                    <li>
+                        <p>Elimina la flacidez del vientre haciéndolo
+                        lucir más plano.  </p>
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="servicios">
+    <div class="container">
+       
+        <div class="center">
+           <h2 class="uptown white">
+               Además...
+           </h2>
+       </div>
+       
+        <div class="flex">
+           <div class="fcentered">
+                   
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/puerco.png" alt="">
+                        <div class="fix-h">
+                            <div class="txt">
+
+                               
+                                <p class="white">Es aplicable en todo el cuerpo </p>
+
+
+                            </div>
+                        </div>
+                        
+                         <a href="https://bodybalance.mx/modelamiento-ondas-choque-cdmx/" class="btn-brown ">¡Saber más!</a>
+                        
+                       
+                    </div>
+
+
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/sosten.png" alt="">
+                        
+                          <div class="fix-h">
+                        
+                            <div class="txt">
+                                
+                                <p class="white">Es reafirmante y revitalizante </p>
+
+
+                            </div>
+                        
+                         </div>
+                           <a href="https://bodybalance.mx/depilaciones-laser-diodo-lightsheer-cdmx/" class="btn-brown">¡Saber más!</a>
+                           
+                    </div>
+
+
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/gluteos.png" alt="">
+                        
+                        
+                        <div class="fix-h">
+                            
+                            <div class="txt">
+                           
+                                <p class="white">
+                                    Levanta y da forma a la zona
+                               </p>
+                                
+                            
+                            </div>
+                            
+                        
+                            
+                        </div>
+                        
+                        <a href="https://bodybalance.mx/tratamientos-faciales-cdmx/" class="btn-brown">¡Saber más!</a>
+                        
+                        
+                    </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+
+<section class="template-section" id="incluye">
+    <div class="container">
+       
+      
+       
+        <div class="flex ">
+           
+           <div class="col col-txt">
+                <h2 class="saqua">¿En cuántas sesiones?
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <p>Son necesarias <b>8 sesiones como mínimo</b> para estimular de manera adecuada la formación de colágeno y elastina </p>
+                
+                <p>Con el paso del tiempo, se favorece una regeneración celular profunda, mejorando la tensión y aspecto de la piel. </p>
+            </div>
+           
+            <div class="col" id="food">
+               
+               <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/BottomChange.jpg" alt="cambio en gluteos">
+               </div>
+               
+            </div>
+           
+            
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+
+
+<section class="template-section" id="obtienes">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="pills">
+               
+               <div class="fcentered">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/Drink.jpg" alt="mujer tomando agua con limón">
+               </div>
+               
+                
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Cuidados posteriores
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul class="bluedots">
+                    <li>
+                        <p>Utilizar pantalla solar FPS 50
+                           (obligatorio). </p>
+                    </li>
+                    <li>
+                        <p>No usar de saunas y/o baños de vapor. </p>
+                    </li>
+                    <li>
+                        <p>Prescribimos los suplementos  si  los necesitas.  </p>
+                    </li>
+                    <li>
+                        <p>Usar fajas no mas allá de un mes. </p>
+                    </li>
+                    <li>
+                        <p>Adecuada hidratación oral con mínimo
+                             3 litros al día. </p>
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="secundarios">
+    <div class="container center">
+
+    
+                <h2 class="saqua">Efectos Secundarios
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                  <p>Eritema (ligero enrojecimiento de la zona tratada, éste desaparece en minutos o en pocas horas.) También puede presentarse, edema, piel frágil, hematoma y dolor.   </p>
+        
+
+    </div>
+</section>
+
+<section class="template-section" id="obtienes">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="pills">
+               
+               <div class="fcentered">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/cicatriz.jpg" alt="mujer con cicatriz">
+               </div>
+               
+                
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Contraindicaciones de
+                este tratamiento:
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul class="bluedots">
+                    <li>
+                        <p>Embarazo y lactancia.</p>
+                    </li>
+                    <li>
+                        <p>Cardiopatías graves, marcapasos o 
+                          válvulas en el corazón. </p>
+                    </li>
+                    <li>
+                        <p>Problemas de coagulación, 
+                       enfermedades del tejido conectivo y 
+                       neuromusculares.  </p>
+                    </li>
+                    <li>
+                        <p>Enfermedades crónicas, cáncer, VIH, 
+                            herpes.</p>
+                    </li>
+                    <li>
+                        <p>Prótesis metálicas o tatuajes con 
+                            metales en los pigmentos.</p>
+                    </li>
+                     <li>
+                        <p>Tratamientos con esteroides o  
+                           inmunosupresores.</p>
+                    </li>
+                     <li>
+                        <p>Dermatitis o psoriasis.
+                            </p>
+                    </li>
+                     <li>
+                        <p>Cirugías recientes o cicatrices 
+                           queloides.</p>
+                    </li>
+                     <li>
+                        <p>Obesidad. </p>
+                    </li>
+                     <li>
+                        <p>Rellenos como el bótox, ácido 
+                       hialurónico o colágeno</p>
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="template-section" id="atendido">
+    <div class="container">
+        <div class="flex">
+            <div class="col fcentered">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whoweare/green-healthy.jpg" alt="Dieta verde, ejercicio y programa">
+            </div>
+            <div class="col fcentered mov-hid">
+                <p>Atendido por médicos especializados en nutrición, nuestro centro es un espacio para perder peso y medidas, sin pasar hambre y sin poner en riesgo tu salud.  </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="queremos">
+    <div class="container">
+        <div class="flex">
+            <div class="col col-txt">
+                <h2 class="">¡Queremos que lo logres!
+                </h2>
+                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <p class="">
+                    Por eso te ayudamos con menús fáciles de cocinar, rápidos y deliciosos, fármacos (si los requieres) y asesoría de ejercicio físico, según tu edad, necesidades y limitaciones.
+                </p>
+                <p class=""><b>Además de la más alta tecnología en tratamientos de modelado corporal, faciales y depilación definitiva. </b></p>
+              
+            </div>
+            <div class="col" id="team">
+                <div class="relative-top">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whoweare/apple-smile.jpg" alt="Chica con manzana, agua y ropa deportiva">
+                </div>
+            </div>
+            
+            <div class="col col-txt fcentered des-hid">
+                <p>Atendido por médicos especializados en nutrición, nuestro centro es un espacio para perder peso y medidas, sin pasar hambre y sin poner en riesgo tu salud.  </p>
+            </div>
+            
+            
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="mision">
+    <div class="container">
+        <div class="flex">
+           
+            <div id="girl-mision" class="col title fcentered">
+                
+            </div>
+            
+            <div class="col col-txt fcentered">
+                <div class="">
+                    <h2 class="uptown">Misión</h2>
+                    <p>Brindar a los pacientes consultas de nutrición clínica integral  para enseñarlos a comer de forma adecuada y lograr sus metas con seguridad sin riesgos en la salud. Ofreciendo al mismo tiempo tecnología de punta en tratamientos complementarios cómo modelado corporal, faciales y depilación definitiva.</p>
+                </div>
+                 
+
+ 
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="template-section" id="vision">
+    <div class="container">
+        <div class="flex">
+           
+         
+            <div class="col col-txt fcentered">
+            <div class="">
+                
+                   <h2 class="uptown">Visión</h2>     
+                    <p>
+                    Ser el primer lugar en México especializado en el tratamiento de clínico integral de la obesidad mediante la nutrición celular, la instalación de hábitos alimenticios y auto cuidado para que nuestros pacientes puedan conseguir resultados efectivos y duraderos, causando un verdadero impacto en la salud.
+                    </p> 
+                
+            </div>
+         
+            </div>
+            
+            <div id="boy-vision" class="col title fcentered">
+                
+            </div>
+            
+            
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="valores">
+    <div class="container">
+        <div class="left">
+            <h2 class="uptown">Valores</h2>
+            <ul>
+                <li>
+                    <p>Honestidad</p>
+                </li>
+
+                <li>
+                    <p>Ética</p>
+                </li>
+
+                <li>
+                    <p>Profesionalismo</p>
+                </li>
+      
+                <li>
+                    <p>Amor</p>
+                </li>
+          
+                <li>
+                    <p>Respeto</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+
+
+<?php 
+    include("shiny-footer.php");
+    get_footer();
+?>
+
+
