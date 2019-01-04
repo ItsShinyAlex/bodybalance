9feec862cf733ac6436edb0bd23c8f38c11444f1
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2019-01-04 16:39:17 -0600

    reto

---
+++ templates/reto.php
@@ -0,0 +1,103 @@
+<?php
+    /* Template Name:  reto*/ 
+echo "<section id ='shiny-template'>";
+    get_header();
+?>
+
+<section class="template-section centered-banner" id="reto">
+      <div class="container">
+        <div class="flex ">
+            <div class="col" >
+                
+            </div>
+            <div class="col" >
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/reto/logo.png" alt="-----">
+                <h2>El único Reto que te premia</h2>
+                <p>Luego de la fiesta es bueno retomar nuestros mejores hábitos y agarrar nuestra salud por el mango. </p>
+                <p class="transformate">Es por eso que te traemos el reto detox Transfórmate 2019.</p>
+                 <a href="https://bodybalance.mx/tienda/reto-transformate/" class="btn-amarillo">¡Apúntame al reto!</a>
+            </div>
+           </div>
+            </div>
+    
+  
+     
+</section>
+<section class="template-section centered-banner" id="beneficios">
+      <div class="container">
+       <h2>¿Cuáles serán sus beneficios?</h2>
+        <div class="flex invertido">
+            <div class="col" >
+                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/reto/bene1.png" alt="-----">
+            </div>
+            <div class="col" >
+               
+                <ul>
+                	<li><span>Reduce las medidas corporales.</span></li>
+                	<li><span>Disminuye tu masa de grasa.</span></li>
+                	<li><span>Potenciarás tus mejores hábitos de alimentación y crearás nuevos.</span></li>
+                	<li><span>Fortalecerás tus músculos.</span></li>
+                	<li><span>Mejorará tu condición física integral.</span></li>
+                </ul>
+            </div>
+           </div>
+            </div>
+    
+  
+     
+</section>
+<section class="template-section centered-banner" id="momento">
+      <div class="container">
+      
+        <div class="flex ">
+            <div class="col" >
+               
+            </div>
+            <div class="col" >
+               
+                <p>Es el momento perfecto para ver resultados saludables y constantes a la vez, pues el organismo viene de un desbalance calórico que aprovecharemos para estabilizar tu peso a la baja.</p>
+                <h2>Iniciamos el <strong>21 de enero</strong></h2>
+                <p class="solo">Y solo hasta el <strong class="quince">15</strong> estará disponible en <strong>$999.00 MXN</strong> luego volverá a su precio regular de <strong>$1350.00 MXN.</strong></p>
+                <a href="https://bodybalance.mx/tienda/reto-transformate/"><p class="dejes">No dejes pasar esta promo que viene con salud incluida.
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/reto/flecha.png" alt="-----"></p></a>
+       
+            </div>
+           </div>
+            </div>
+    
+  
+     
+</section>
+
+
+
+ <?php echo do_shortcode('[text_block id=1112 description="faq_transformate"]'); ?>
+ 
+ 
+<section class="template-section centered-banner" id="sesiones">
+      <div class="container">
+      
+        <div class="flex">
+            <div class="col" >
+                
+            </div>
+            <div class="col" >
+               
+               <h2>Sesiones de Live privadas</h2>
+               <p class="tendremos">Tendremos  <strong>tres sesiones en vivo durante nuestro reto donde aprenderás: </strong></p>
+               <p>Las diferencias entre grasa y músculo y cómo aprovechar estas diferencias para bajar o subir de peso de forma saludable.</p>
+               <p class="ventaja">Las ventajas de dormir bien y cómo lograrlo para perder grasa Y poner el cuerpo a tu favor.</p>
+               <a href="https://bodybalance.mx/tienda/reto-transformate/" class="btn-amarillo">¡Apúntame al reto!</a>
+            </div>
+           </div>
+            </div>
+    
+  
+     
+</section>
+
+<?php 
+    include("shiny-footer.php");
+    get_footer();
+?>
+
