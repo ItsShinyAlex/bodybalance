ee7d89601dc8f8ab206e35dc97630e63a999604d
Jesus <jesus@esbrillante.mx>
2019-01-22 10:21:06 -0600

    Landings para ADS
    Creación de landings para ADS

---
+++ footer-landing.php
@@ -0,0 +1,144 @@
+<section id="contacto">
+    <div class="container">
+        <div class="flex">
+            <div class="col c-col">
+                <h2>
+                  ¡Contáctanos
+                   <br>
+                    <span class="uptown saqua">
+                        Hoy mismo!
+                    </span>
+                </h2>
+                
+                <div id="cform">
+                     <!--Ajuste de formulario segun la página Web -->
+                     <?php 
+                        if(is_page_template('landing-ads/consulta.php')){
+                            echo do_shortcode('[caldera_form id="CF5c4696b8b6d43"]');
+                        }
+                        else if(is_page_template('landing-ads/depilacion.php')){
+                            echo do_shortcode('[caldera_form id="CF5c469b661f564"]');
+                        }
+                         else if(is_page_template('landing-ads/consulta-online.php')){
+                           echo do_shortcode('[caldera_form id="CF5c469ca24fe7d"]'); 
+                        }
+                    
+                      else if(is_page_template('landing-ads/ondasdechoque.php')){
+                           echo do_shortcode('[caldera_form id="CF5c46ba6bc93e3"]'); 
+                        }
+                    
+                        
+                     ?>
+                </div>
+                
+                
+                <div id="responsable" class="" >
+                    <p>Responsable sanitario: </p> <p> Dra. Berenice Antonia Ramírez Solís</p>
+                    <p>Cédula: 10570872</p>
+                    <p>IPN</p>
+                </div>
+                
+            </div>
+            
+            
+            
+            <div class="col" id="mapa">
+
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered ">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whatsapp.png" alt="WhatsApp">
+                    </div>
+                    <div class="col txt ">
+                        <p class="white phone">
+                            <a class="white" href="https://wa.me/525519587101">5519587101</a><br>
+                        </p>
+                    </div>
+                </div>
+               
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered ">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/phone.png" alt="teléfono">
+                    </div>
+                    <div class="col txt">
+                        <p class="white phone">
+                            <a class="white" href="tel:5513282907">01 (55) 1328 2907</a>
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/clock.png" alt="horario">
+                    </div>
+                    <div class="col txt fcentered">
+                        <p >Lunes a Viernes de 9 a.m. 8 p.m. 
+                        Sábado de 8 a.m. a  5 p.m.</p>
+                    </div>
+                </div>
+                
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ubicacion.png" alt="ubicación">
+                    </div>
+                    <div class="col txt fcentered">
+                                         
+                        <p>
+                            Torre Médica Tuxpan Núm. 8, Col Roma Sur, Ciudad de México (CDMX), Piso 2 C.P 06760 Estamos a 40 metros del Metro Chilpancingo
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="flex mar-bot" id="followus">
+                    <div class="col txt fcentered" id="siguenos">
+                       <a href="https://www.facebook.com/bodybalance.cdmx/" target="_blank"><p><b>SÍGUENOS</b></p></a>
+                        
+                    </div>
+                    <div class="col icon fcentered">
+                       <a href="https://www.facebook.com/bodybalance.cdmx/" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/face.png" alt="Facebook"></a>
+                       
+                    </div>
+                    <div class="col icon fcentered " id="youtube">
+                       <a href="https://www.youtube.com/channel/UCnfO3P1oyAz3mT7ZQdSugUg" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube-symbol.svg" alt="YouTube"></a>
+                       
+                    </div>
+                    <div class="col icon fcentered">
+                       <a href="https://www.instagram.com/bodybalance.mx/?hl=es-la" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/instagram-logo.png" alt="Instagram"></a>
+                       
+                    </div>
+                </div>
+                
+                <div class="mar-bot pad-left">
+                    <a href="https://goo.gl/maps/GapxsnfqX7y" class="btn-saqua" target="_blank">¿Cómo llegar?</a>    
+                </div>
+                
+                
+                <div class="fcentered">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d940.7823156044791!2d-99.16812294669649!3d19.406820165643005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff154ab64aa3%3A0xe5ddf2cd58d48680!2sTuxpan+8%2C+Roma+Sur%2C+06760+Ciudad+de+M%C3%A9xico%2C+CDMX!5e0!3m2!1ses-419!2smx!4v1536244758591" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+            
+            
+            
+        </div>
+        
+                       
+    </div>
+    
+
+
+</section>
+
+    <!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?5zroulJQaFiw4Kp6m95x6tNrEkQAKDO0";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
+
+</section>
+
+
+<?php wp_footer() ?>
---
+++ landing-ads/consulta-online.php
@@ -0,0 +1,275 @@
+<?php
+    /* Template Name:  Consulta Online ADS*/ 
+echo "<section id ='shiny-template'>";
+    get_header();
+?>
+
+
+
+
+<section class="template-section" id="tumeta-online">
+    
+    <div class="container desktop-r-space fcentered col-dir">
+      
+       <div class="fcentered h1-contain">
+           
+           
+            <h1 class="saqua uptown">¡La nutrición que 
+            <span class="">necesitas está a un clic! </span></h1>
+           
+       </div>
+       
+            
+        <div class="fcentered col-dir">
+            <div class="w-box">
+            <p>En nuestra consulta médico nutricional online analizamos  estilo de vida, gustos y necesidades, para darte la mejor opción de tratamiento desde la comodidad de tu casa. 
+              <br>
+                <br>
+                 <b>Y está disponible para toda la República Mexicana via:</b>
+              </p>
+              
+              <div class="desktop-hide fcentered">
+                <a href="https://bodybalance.mx/consulta-medico-nutricional-online/?add-to-cart=333" class="btn-brown">¡ME INTERESA!</a>
+            </div>
+              
+                <div id="call-icons">
+              
+                        <div class="flex">
+                            <div class="col fcentered">
+                                <a href="https://m.me/bodybalance.cdmx?ref=consultaonline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/001-skype.png" alt="Skype"></a>
+                            </div>    
+                            <div class="col fcentered">
+                                <a href="https://wa.me/525519587101"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/002-whatsapp.png" alt="WhatsApp"></a>
+                            </div>
+                            <div class="col fcentered">
+                                <a href="tel:5513282907"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/003-mobile-phone.png" alt="Teléfono"></a>
+                            </div>
+                            
+                              <div class="col btn-col fcentered mov-hid">
+                                <a href="https://bodybalance.mx/consulta-medico-nutricional-online/?add-to-cart=333" class="btn-saqua" target="_blank">¡COMPRAR!</a>
+                            </div>
+                            
+                        </div>
+                </div>
+            </div>
+            
+        </div>
+        
+      
+        
+        
+        
+    </div>
+     
+</section>
+
+<section class="template-section" id="obtienes">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="online-food">
+                
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Beneficios <span class="mov-hid">de tener una Nutrióloga en Línea </span> </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul>
+                    <li>
+                        <p>Recibirás atención y asesoría médica profesional para perder peso sin salir de casa y sin riesgos para tu salud.  </p>
+                    </li>
+                    <li>
+                        <p>Creamos para ti un menú totalmente personalizado con comidas sanas,  fáciles de preparar y deliciosas. </p>
+                    </li>
+                    <li>
+                        <p>Prescribimos los suplementos si los necesitas.    </p>
+                    </li>
+                    <li>
+                        <p>Te orientamos en cuanto a la actividad física que puedes y necesitas realizar.  </p>
+                    </li>
+                    <li>
+                        <p>Realizamos educación nutrimental para ir establecer contigo hábitos alimenticios permanentes, que te ayuden a obtener resultados duraderos.</p>
+                    </li>
+                    <li>
+                        <div class="mcwidget-embed" data-widget-id="3094365"></div>
+
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="video">
+    
+    <div class="container ">
+      
+       <div class="fcentered">
+           
+           
+            <h2 class="saqua uptown">¿Cómo Funciona?</h2>
+           
+       </div>
+       
+       <div class="fcentered">
+           <iframe width="560" height="315" src="https://www.youtube.com/embed/tW8xC-0IAzU?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+       </div>
+       
+            
+   
+        
+    </div>
+     
+</section>
+
+<section class="template-section mov-hid" id="incluye">
+    <div class="container">
+        <div class="flex ">
+           
+           <div class="col col-txt">
+                <h2 class="saqua">¿Qué incluye la consulta nutricional en línea?
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul>
+             
+                    <li>
+                        <p>Plan de alimentación personalizado.
+                         </p>
+                    </li>
+                    <li>
+                        <p>Análisis de medidas antropométricas.
+                        </p>
+                    </li>
+                    <li>
+                        <p>Plicometria o toma de pliegues cutáneos. </p>
+                    </li>
+                    <li>
+                        <p>Historial clínico médico-nutricio.</p>
+                    </li>
+                     <li>
+                        <p>Análisis de tipo de cuerpo y de composición corporal.</p>
+                    </li>
+                     <li>
+                        <p>Análisis de estilo de vida.
+                        </p>
+                    </li>
+                     <li>
+                        <p>Plan de alimentación personalizado de acuerdo a requerimientos y objetivos.</p>
+                    </li>
+                     <li>
+                        <p>Recomendaciones generales de entrenamiento.</p>
+                    </li>
+                     <li>
+                        <p>Catálogo de compras saludables.</p>
+                    </li>
+                     <li>
+                        <p>Lista de súper.</p>
+                    </li>
+                    <li>
+                        <p>Asesoría de suplementación (en caso de ser necesaria).</p>
+                    </li>
+                    <li>
+                        <p>Seguimiento y asesoría continúa vía e-mail.</p>
+                    </li>
+                    <li>
+                        <p>Toma de signos vitales.</p>
+                    </li>
+                </ul>
+            </div>
+           
+            <div class="col" id="food">
+                
+            </div>
+           
+            
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+<section  class="plantilla-seccion" id="money">
+  <div class="container">
+      
+       <div class="relative-top">
+           
+           <h2 id="clock-in" class="uptown center">
+              ¡Logra tu meta
+                <br>
+               <span class="big">
+                   sin salir de casa!
+               </span>
+           </h2>
+            
+            <div class="flex">
+           
+                <div class="col col-txt">
+                   <div class="fcentered">
+                       
+                    <ul>
+                       <p class="listar-servicios2"><?php echo do_shortcode('[vc_row][vc_column][dt_products_carousel show_products="categories_products" order="asc" orderby="title" dis_posts_total="1" slides_on_wide_desk="1" slides_on_desk="1" slides_on_lapt="1" slides_on_h_tabs="1" slides_on_v_tabs="1" item_space="0" speed="0" arrows="n" category_ids="42" css_dt_blog_carousel=".vc_custom_1537813311573{padding-right: 40px !important;padding-left: 40px !important;}"][/vc_column][/vc_row]');
+    ?></p>
+                        <!--<li>
+                            <p><b>Costo de primera Consulta Médico Nutricional: </b><br>650 MXN</p>
+                        </li>
+                        <li>
+                            <p><b>Consecutivas:</b><br>600 MXN</p>
+                        </li>-->
+                    </ul>
+                    
+                    
+                   </div>
+                   
+                   <div class="fcentered">
+                       
+                   </div>
+                    
+                </div>
+                
+                <div class="col">
+                   <div class="fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/money.png" alt="Dinero en efectivo">
+                   </div>
+                   
+                </div>
+                
+            </div>
+       </div>
+   
+  </div>
+</section>
+
+<section class="template-section" id="howtopay">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="pills">
+                <div class="fcentered"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/creditcard.png" alt="Tarjeta de crédito"></div>
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="">¿Cómo Pagar?
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <p>
+                    Efectivo, debito, crédito. 3 meses sin intereses todas las tarjetas de crédito y todos los bancos.
+                </p>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<?php    
+    get_footer('landing');
+?>
+
