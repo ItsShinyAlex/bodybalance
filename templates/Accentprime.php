3ba2ee63065231779a5cc2bd4702df2006a0738c
Lyngall <soporte@esbrillante.com>
2019-03-15 05:30:09 -0600

    Hasta acent paquetes

---
+++ templates/Accentprime.php
@@ -0,0 +1,554 @@
+<?php
+    /* Template Name: ACCENTPRIME */ 
+echo "<section id ='shiny-template'>";
+    get_header();
+?>
+
+
+
+
+<section class="template-section centered-banner" id="slider-accentprime">
+    
+  
+     
+</section>
+
+<section id="accent-prime" class="template-section centered-and-title aquaback">
+    
+    <h2>Accent Prime </h2>
+    
+    <p class="centered">
+        Este tratamiento combina la más avanzada tecnología de ultrasonidos y radiofrecuencia para <b> remodelar tu cuerpo y rostro,</b> tensando la piel,  reduciendo 
+        <span class="brown">
+            la grasa localizada y la celulitis
+        </span>
+         entre otras mejoras estéticas para ofrecerte un tratamiento <b>   personalizado, cómodo y rápido   </b> <span class="brown"> con resultados duraderos.  </span>  
+    </p>
+    
+</section>
+
+<section class="template-section r-list darkback" id="accent-how-it-works">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="">
+                <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/accentprime/comoFunciona.jpg" alt="Bodybalance, maquina para accent prime">
+                </div>
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">¿Cómo funciona?
+                </h2>
+
+                <p>
+                    Las ondas electromagnéticas de alta potencia, producen una rápida oscilación y fricción de las moléculas de agua, provocando un calentamiento volumétrico uniforme en el área de tratada. 
+                </p>
+                <p>
+                    
+                    Este  calor genera una contracción volumétrica de la piel que contrae las fibras existentes y estimula la formación de nuevo colágeno. 
+                    
+                </p>
+                <p>
+                    
+                    Con la radiofrecuencia se <b>logra acelerar el metabolismo de las células grasas</b>, provocando una disminución importante de su tamaño y a su vez, de los nódulos de la celulitis.
+
+                    
+                </p>
+                <p>
+                    
+                    Y para ofrecerte mejores resultados permite “personalizar” y controlar el nivel  de profundidad en las capas del tejido a las que se desea llegar (superficial -epidermis-, medio -dermis-  y profundo -hipodermis-) para focalizar el calor según el tratamiento a realizar. 
+                    
+                </p>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+<section class="template-section r-list" id="accent-beneficios">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="">
+                <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/accentprime/gluteos.jpg" alt="Gluteos">
+                </div>
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Beneficios para tu cuerpo
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul>
+                    <li>
+                        <p>Tensa la piel y estimula la producción 
+                          de colágeno. </p>
+                    </li>
+                    <li>
+                        <p>Mejora el grosor y la alineación de la
+                        piel.</p>
+                    </li>
+                    <li>
+                        <p>Modela el contorno corporal.</p>
+                    </li>
+                    <li>
+                        <p>Reduce la grasa localizada. </p>
+                    </li>
+                    <li>
+                        <p>Disminuye y controla la celulitis. </p>
+                    </li>
+                    <li>
+                        <p>Previene la flacidez.</p>
+                    </li>
+                    <li>
+                        <p>Contribuye en la Pre y Post liposucción 
+                           y abdominoplastia. </p>
+                    </li>
+                    <li>
+                        <p>Reduce las estrías y cicatrices</p>
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+<section id="listar-servicios" class="plantilla-seccion hide">
+   <h2 class="center uptown">Zonas de aplicación <br> <span class="normal">y precio por sesión</span> </h2>
+    <?php echo do_shortcode('[vc_row][vc_column][dt_products_carousel show_products="categories_products" order="asc" orderby="title" arrow_icon_size="25px" arrow_bg_width="36x" arrow_border_width="0px" arrows_bg_show="n" arrow_icon_color_hover="rgba(0,0,0,0.75)" arrow_bg_color_hover="#34afbc" r_arrow_icon_paddings="0px 0px 0px 0px" r_arrow_v_offset="0px" l_arrow_icon_paddings="0px 0px 0px 0px" l_arrow_v_offset="0px" category_ids="17"][/vc_column][/vc_row]');
+    ?>
+    
+    <div class="center">
+        *Se recomienda un mínimo 4 sesiones para resultados <br>
+            considerables y duraderos.  
+    </div>
+    
+</section>
+
+<section id="special-packs" class="template-section brownback">
+    
+    <div class="container">
+        <div class="centered">
+            
+            
+
+                <h2>Paquetes Especiales </h2>
+                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <p class="centered">
+                   ¡Obtén más sesiones por menos dinero!
+                </p>
+                <a href="" class="btn-saqua" target="_blank">¡Ver Paquetes!</a>
+            
+        </div>
+    </div>
+   
+    
+</section>
+
+<section class="template-section columns3 mov-hide" id="ademas">
+    <div class="container">
+       
+        <div class="center">
+           <h2 class="uptown white">
+               Además...
+           </h2>
+       </div>
+       
+        <div class="flex">
+           <div class="fcentered">
+                   
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/puerco.png" alt="">
+                        <div class="fix-h">
+                            <div class="txt">
+
+                               
+                                <p class="white">Es aplicable en todo el cuerpo </p>
+
+
+                            </div>
+                        </div>
+                        
+                         <a href="https://bodybalance.mx/modelamiento-ondas-choque-cdmx/" class="btn-brown hide">¡Saber más!</a>
+                        
+                       
+                    </div>
+
+
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/sosten.png" alt="">
+                        
+                          <div class="fix-h">
+                        
+                            <div class="txt">
+                                
+                                <p class="white">Es reafirmante y revitalizante </p>
+
+
+                            </div>
+                        
+                         </div>
+                           <a href="https://bodybalance.mx/depilaciones-laser-diodo-lightsheer-cdmx/" class="btn-brown hide">¡Saber más!</a>
+                           
+                    </div>
+
+
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/gluteos.png" alt="">
+                        
+                        
+                        <div class="fix-h">
+                            
+                            <div class="txt">
+                           
+                                <p class="white">
+                                    Levanta y da forma a la zona
+                               </p>
+                                
+                            
+                            </div>
+                            
+                        
+                            
+                        </div>
+                        
+                        <a href="https://bodybalance.mx/tratamientos-faciales-cdmx/" class="btn-brown hide">¡Saber más!</a>
+                        
+                        
+                    </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+
+
+<section class="template-section columns3 desktop-hide" id="ademas">
+    <div class="container">
+       
+        <div class="center">
+           <h2 class="uptown white">
+               Además...
+           </h2>
+       </div>
+       
+        <div class="flex">
+           <div class="fcentered">
+                   
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/puerco.png" alt="">
+                        <div class="fix-h">
+                            <div class="txt">
+
+                               
+                                <p class="white">Es aplicable en todo el cuerpo </p>
+
+
+                            </div>
+                        </div>
+                        
+                       
+                  </div>
+            </div>
+        </div>    
+
+        <div class="flex">
+           <div class="fcentered">
+
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/sosten.png" alt="">
+                        
+                          <div class="fix-h">
+                        
+                            <div class="txt">
+                                
+                                <p class="white">Es reafirmante y revitalizante </p>
+
+
+                            </div>
+                        
+                         </div>
+                           <a href="https://bodybalance.mx/depilaciones-laser-diodo-lightsheer-cdmx/" class="btn-brown hide">¡Saber más!</a>
+                           
+                    </div>
+                    
+                    
+        </div>
+        </div>  
+        
+        <div class="flex">
+           <div class="fcentered">
+
+
+                    <div class="col center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/gluteos.png" alt="">
+                        
+                        
+                        <div class="fix-h">
+                            
+                            <div class="txt">
+                           
+                                <p class="white">
+                                    Levanta y da forma a la zona
+                               </p>
+                                
+                            
+                            </div>
+                            
+                        
+                            
+                        </div>
+                        
+                        <a href="https://bodybalance.mx/tratamientos-faciales-cdmx/" class="btn-brown hide">¡Saber más!</a>
+                        
+                        
+                    </div>
+                    
+                    
+                    
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<section class="template-section llist" id="cuantas-sesiones">
+    <div class="container">
+       
+      
+       
+        <div class="flex ">
+           
+           <div class="col col-txt">
+                <h2 class="saqua">¿En cuántas sesiones?
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <p>Son necesarias <b>8 sesiones como mínimo</b> para estimular de manera adecuada la formación de colágeno y elastina </p>
+                
+                <p>Con el paso del tiempo, se favorece una regeneración celular profunda, mejorando la tensión y aspecto de la piel. </p>
+            </div>
+           
+            <div class="col" id="">
+               
+               <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/BottomChange.jpg" alt="cambio en gluteos">
+               </div>
+               
+            </div>
+           
+            
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+
+
+<section class="template-section r-list" id="cuidados">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="">
+               
+               <div class="fcentered">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/Drink.jpg" alt="mujer tomando agua con limón">
+               </div>
+               
+                
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Cuidados posteriores
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
+                <ul class="bluedots">
+                    <li>
+                        <p>Utilizar pantalla solar FPS 50
+                           (obligatorio). </p>
+                    </li>
+                    <li>
+                        <p>No usar de saunas y/o baños de vapor. </p>
+                    </li>
+                    <li>
+                        <p>Prescribimos los suplementos  si  los necesitas.  </p>
+                    </li>
+                    <li>
+                        <p>Usar fajas no mas allá de un mes. </p>
+                    </li>
+                    <li>
+                        <p>Adecuada hidratación oral con mínimo
+                             3 litros al día. </p>
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="secundarios">
+    <div class="container center">
+
+            <div class="fcentered">
+                <h2 class="saqua">Efectos Secundarios
+                </h2>
+            </div>
+                
+                  <p>Eritema (ligero enrojecimiento de la zona tratada, éste desaparece en minutos o en pocas horas.) También puede presentarse, edema, piel frágil, hematoma y dolor.   </p>
+        
+
+    </div>
+</section>
+
+<section class="template-section r-list" id="contra">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="">
+               
+               <div class="fcentered">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/cicatriz.jpg" alt="mujer con cicatriz">
+               </div>
+               
+                
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Contraindicaciones de
+                este tratamiento:
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio/b-line.png" alt="-----">
+                <ul class="bluedots">
+                    <li>
+                        <p>Embarazo y lactancia.</p>
+                    </li>
+                    <li>
+                        <p>Cardiopatías graves, marcapasos o 
+                          válvulas en el corazón. </p>
+                    </li>
+                    <li>
+                        <p>Problemas de coagulación, 
+                       enfermedades del tejido conectivo y 
+                       neuromusculares.  </p>
+                    </li>
+                    <li>
+                        <p>Enfermedades crónicas, cáncer, VIH, 
+                            herpes.</p>
+                    </li>
+                    <li>
+                        <p>Prótesis metálicas o tatuajes con 
+                            metales en los pigmentos.</p>
+                    </li>
+                     <li>
+                        <p>Tratamientos con esteroides o  
+                           inmunosupresores.</p>
+                    </li>
+                     <li>
+                        <p>Dermatitis o psoriasis.
+                            </p>
+                    </li>
+                     <li>
+                        <p>Cirugías recientes o cicatrices 
+                           queloides.</p>
+                    </li>
+                     <li>
+                        <p>Obesidad. </p>
+                    </li>
+                     <li>
+                        <p>Rellenos como el bótox, ácido 
+                       hialurónico o colágeno</p>
+                    </li>
+                </ul>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<section class="template-section pad-top-section dark-back" id="howtopay">
+    <div class="container">
+        <div class="flex ">
+            <div class="col" id="pills">
+                <div class="fcentered"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/creditcard.png" alt="Tarjeta de crédito"></div>
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="">¿Cómo Pagar?
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/w-bar.png" alt="-----">
+                <p>
+                    Efectivo, debito, crédito. 3 meses sin intereses todas las tarjetas de crédito y todos los bancos.
+                </p>
+            </div>
+            
+            
+           
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+    include("shiny-footer.php");
+    get_footer();
+?>
+
+
