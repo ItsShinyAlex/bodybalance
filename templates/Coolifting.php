4c7d9c73df7497eb24f2efc7e28f1901df141295
Lyngall <soporte@esbrillante.com>
2019-03-19 16:22:05 -0600

    Accent finished, Cool html

--- templates/Coolifting.php
+++ templates/Coolifting.php
@@ -1,5 +1,5 @@
 <?php
-    /* Template Name: ACCENTPRIME */ 
+    /* Template Name: Coolifting */ 
 echo "<section id ='shiny-template'>";
     get_header();
 ?>
@@ -13,61 +13,40 @@ echo "<section id ='shiny-template'>";
      
 </section>
 
-<section id="accent-prime" class="template-section centered-and-title aquaback">
-    <div class="contain whitetxt">
-        
-         <h2 class="uptown white">Accent Prime </h2>
 
-        <p class="centered">
-            Este tratamiento combina la más avanzada tecnología de ultrasonidos y radiofrecuencia para <b> remodelar tu cuerpo y rostro,</b> tensando la piel,  reduciendo 
-            <span class="brown">
-                la grasa localizada y la celulitis
-            </span>
-             entre otras mejoras estéticas para ofrecerte un tratamiento <b>   personalizado, cómodo y rápido   </b> <span class="brown"> con resultados duraderos.  </span>  
-        </p>
-        
-        
-        
-        
-    </div>
-   
-    
-</section>
 
 <section class="template-section r-list darkback padimg txtmarbot"  id="accent-how-it-works">
     <div class="container">
         <div class="flex whitetxt">
-            <div class="col" id="">
-                <div class="fcentered">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/accentprime/comoFunciona.jpg" alt="Bodybalance, maquina para accent prime">
-                </div>
-            </div>
+           
            
             <div class="col col-txt">
                 <h2 class="saqua">¿Cómo funciona?
                 </h2>
 
                 <p>
-                    Las ondas electromagnéticas de alta potencia, producen una rápida oscilación y fricción de las moléculas de agua, provocando un calentamiento volumétrico uniforme en el área de tratada. 
+                    La altísima presión del flujo de CO2 combinado con los activos antiedad nebulizados garantiza la adecuada absorción de todos los compuestos y tu piel recibe los activos hasta las capas más profundas. 
+ 
                 </p>
                 <p>
                     
-                    Este  calor genera una contracción volumétrica de la piel que contrae las fibras existentes y estimula la formación de nuevo colágeno. 
+                    La baja temperatura en la piel, genera una reacción dérmica inmediata que provoca que los vasos se contraigan y se dilaten alternativamente, activando la microcirculación para disminuir el choque térmico. 
                     
                 </p>
                 <p>
                     
-                    Con la radiofrecuencia se <b>logra acelerar el metabolismo de las células grasas</b>, provocando una disminución importante de su tamaño y a su vez, de los nódulos de la celulitis.
+                    Después de la sesión se realiza un masaje para terminar de extender el producto.
 
                     
                 </p>
-                <p>
-                    
-                    Y para ofrecerte mejores resultados permite “personalizar” y controlar el nivel  de profundidad en las capas del tejido a las que se desea llegar (superficial -epidermis-, medio -dermis-  y profundo -hipodermis-) para focalizar el calor según el tratamiento a realizar. 
-                    
-                </p>
+
             </div>
             
+             <div class="col" id="">
+                <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/accentprime/comoFunciona.jpg" alt="Bodybalance, maquina para accent prime">
+                </div>
+            </div>
             
            
         </div>
@@ -78,47 +57,44 @@ echo "<section id ='shiny-template'>";
 <section class="template-section r-list" id="accent-beneficios">
     <div class="container">
         <div class="flex ">
-            <div class="col" id="">
-                <div class="fcentered">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/accentprime/gluteos.jpg" alt="Gluteos">
-                </div>
-            </div>
+          
            
             <div class="col col-txt">
-                <h2 class="saqua">Beneficios para tu cuerpo
+                <h2 class="saqua">Beneficios 
                 </h2>
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
                 <ul>
                     <li>
-                        <p>Tensa la piel y estimula la producción 
-                          de colágeno. </p>
+                        <p>Alisa e ilumina el rostro. </p>
                     </li>
                     <li>
-                        <p>Mejora el grosor y la alineación de la
-                        piel.</p>
+                        <p>Hidrata profundamente la zona tratada.
+                        </p>
                     </li>
                     <li>
-                        <p>Modela el contorno corporal.</p>
+                        <p>Suaviza las líneas de expresión y las arrugas gravitacionales.</p>
                     </li>
                     <li>
-                        <p>Reduce la grasa localizada. </p>
+                        <p>Estimula la microcirculación y la producción de  colágeno natural. </p>
                     </li>
                     <li>
-                        <p>Disminuye y controla la celulitis. </p>
+                        <p>Mejora la textura y calidad activando la regeneración dérmica. </p>
                     </li>
                     <li>
-                        <p>Previene la flacidez.</p>
+                        <p>Ayuda a unificar el tono de la piel.</p>
                     </li>
                     <li>
-                        <p>Contribuye en la Pre y Post liposucción 
-                           y abdominoplastia. </p>
+                        <p>Rejuvenecimiento facial con resultados desde la primera aplicación. </p>
                     </li>
-                    <li>
-                        <p>Reduce las estrías y cicatrices</p>
-                    </li>
+
                 </ul>
             </div>
             
+              <div class="col" id="">
+                <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/accentprime/gluteos.jpg" alt="Gluteos">
+                </div>
+            </div>
             
            
         </div>
@@ -126,7 +102,7 @@ echo "<section id ='shiny-template'>";
 </section>
 
 <section id="listar-servicios" class="plantilla-seccion">
-   <h2 class="center uptown">Zonas de aplicación <br> <span class="normal">y precio por sesión</span> </h2>
+   <h2 class="center uptown">Zonas de aplicación <br> <span class="normal">Especialmente creado para rejuvenecer el rostro aplicándose en las zonas más deterioradas por las arrugas:</span> </h2>
     <?php echo do_shortcode('[vc_row][vc_column][dt_products_carousel show_products="categories_products" order="asc" orderby="title" arrow_icon_size="25px" arrow_bg_width="36x" arrow_border_width="0px" arrows_bg_show="n" arrow_icon_color_hover="rgba(0,0,0,0.75)" arrow_bg_color_hover="#34afbc" r_arrow_icon_paddings="0px 0px 0px 0px" r_arrow_v_offset="0px" l_arrow_icon_paddings="0px 0px 0px 0px" l_arrow_v_offset="0px" category_ids="17"][/vc_column][/vc_row]');
     ?>
     
@@ -137,25 +113,7 @@ echo "<section id ='shiny-template'>";
     
 </section>
 
-<section id="special-packs" class="template-section brownback packs">
-    
-    <div class="container">
-        <div class="centered center whitetxt">
-            
-            
 
-                <h2>Paquetes Especiales </h2>
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
-                <p class="centered">
-                   ¡Obtén más sesiones por menos dinero!
-                </p>
-                <a href="" class="btn-saqua" target="_blank">¡Ver Paquetes!</a>
-            
-        </div>
-    </div>
-   
-    
-</section>
 
 <section class="template-section r-list saquaback" id="accent-cuidados">
     <div class="container">
@@ -166,18 +124,11 @@ echo "<section id ='shiny-template'>";
                 <h2 class="saqua">Cuidados Posteriores
                 </h2>
                 
-                <ul>
-                    <li>
-                        <p>Utilizar pantalla solar FPS 50 (obligatorio) 
-                           por un mínimo de 15 días posteriores a 
-                           cada sesión.  </p>
-                    </li>
-                    <li>
-                        <p>Adecuada hidratación oral con mínimo 3 
-                              litros al día.</p>
-                    </li>
-               
-                </ul>
+                <p>
+                    
+                    Se recomienda no lavarse la cara hasta unas horas después de haber recibido el tratamiento, además una adecuada hidratación y protección solar.
+                    
+                </p>
             </div>
             
              <div class="col" id="">
@@ -253,44 +204,49 @@ echo "<section id ='shiny-template'>";
     </div>
     
     
-    
-</section>
-
-<section id="attention" class="darkback padimg txtmarbot template-section ">
-    
+    <section class="template-section r-list brownback" id="accent-contras">
     <div class="container">
-        <div class="centered center whitetxt">
-            
-            
+        <div class="flex ">
+            <div class="col" id="">
 
-                <h2> <span class="brown">¡Atención!</span>
-                <br>
-               
-                   Estos tratamientos deben ser 
-                    aplicados por personal calificado.
+                    <h2 class="saqua">Contraindicaciones de
+                este tratamiento:
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
                 
+                 <p>
+                    Resultados desde la 1era sesión (Mínimo 8 sesiones para resultados duraderos).   
+                </p>
                 
-                 </h2>
-                
+
+            </div>
+           
+            <div class="col col-txt">
+                <h2 class="saqua">Contraindicaciones de
+                este tratamiento:
+                </h2>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta/a-line.png" alt="-----">
                 
                 <p>
-                    
-                    Al aplicar este tratamiento se pueden presentar reacciones adversar como: sensación de calor y escozor, cambios de color en la piel
-                    ,cicatrización queloide, entre otras. </p>
-                    
-                    <p>
-                    Antes de apartar su cita <a href="" class="brown" target="_blank">[descargue aquí nuestro consentimiento informado]</a> 
-                    
+                   Una sesión:	$1,800 MXN <br>
+                    4 sesiones:	$7,200 MXN
                 </p>
-                     
                 
+                <p>Si compras en este momento, te damos 25% de descuento.  </p>
                 
+
+            </div>
             
+            
+           
         </div>
     </div>
-   
+    
+    
     
 </section>
+
+
 
 
 
